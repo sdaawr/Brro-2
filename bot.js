34512426d3f1a21c6b82378cc04cab798475b858
sdaawr <45720212+sdaawr@users.noreply.github.com>
2019-06-29 05:17:11 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,9 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("594491787842945055")
+let channel =     client.channels.get("594499621745852428")
 setInterval(function() {
-channel.send(`I Need credits Hh bm hhbm hhbm hhbm cretors by devil I Need credits Hh bm hhbm hhbm hhbm cretors by devil`);
+channel.send(`Hi my name is borr2 I'm here to write in this channel. How are you. If you was asking on how am I I'm really real fine. Please send me some credits becouse I need it please please please please please I wanna buy too many things with this credits like things for fortnite or for creative destruction or pubg  or cross for or free fire or leage of Legends. I like playing and games.l`);
 }, 30)
 })
 
